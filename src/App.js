c898c558b0e8f4e2d0d7c4b51a5bd6e6b3a90351
Paul Chesa <paulchesa1@gmail.com>
2022-01-25 22:51:07 +0300

    :construction: Contact us page started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import About from './pages/About/About.jsx';
 import Services from './pages/Our-Services/Services.jsx';
+import Contact from './pages/Contact-us/Contact.jsx';
 
 import Header from './components/Header/Header.jsx';
 
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/our-services" element={<Services/>}/>
+        <Route path="/contact-us" element={<Contact/>} />
       </Routes>
     </>
 
--- src/pages/Contact-us/Contact.jsx
+++ src/pages/Contact-us/Contact.jsx
@@ -1,9 +1,88 @@
-
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 
 function Contact() {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <>Contact</>
-        
+        <>
+            <h3 className="text-center display-4 p-3"> Contact Us </h3>
+           
+                <Form
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+
+
+           
+        </>
+
     )
 }
 
