27060ca04ebccdc2cde78474709786e86a5d8e74
Paul Chesa <paulchesa1@gmail.com>
2022-01-26 05:56:44 +0300

    :art: clients page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 
-
 import Home from './pages/Home/Home.jsx';
 import About from './pages/About/About.jsx';
 import Services from './pages/Our-Services/Services.jsx';
 import Contact from './pages/Contact-us/Contact.jsx';
+import Clients from './pages/OurClients/Clients.jsx';
 
 import Header from './components/Header/Header.jsx';
 
@@ -19,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/our-services" element={<Services/>}/>
         <Route path="/contact-us" element={<Contact/>} />
+        <Route path="/clients" element={<Clients/>} />
       </Routes>
     </>
 
---
+++ src/pages/OurClients/Clients.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import clients from '../../assets/our-clients.png';
+
+function Clients() {
+    return (
+        <>
+            <h3 className="text-center display-4 p-3">Our Clients</h3>
+            <div className="container d-flex justify-content-center align-items-center">
+               
+                        <img className="clients-img" src={clients} alt="clients-placeholder" />
+                  
+            </div>
+        </>
+    )
+}
+
+export default Clients
